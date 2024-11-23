8aaf9547f1ccb7c25ff159e8aae3c4e54c91c904
404ISU <112893915+404ISU@users.noreply.github.com>
2024-11-23 07:42:34 +0300

    Add files via upload

---
+++ client/src/components/RequestList/RequestList.js
@@ -0,0 +1,13 @@
+// src/components/RequestList.js
+import React from 'react';
+
+const RequestList = () => {
+    return (
+        <div>
+            <h2>Saved Requests</h2>
+            {/* Здесь будет логика для отображения сохраненных запросов */}
+        </div>
+    );
+};
+
+export default RequestList;
